0580b38c2d2502bea5d489da07c67a80c936b41a
niteshg97 <166297874+niteshg97@users.noreply.github.com>
2026-09-16 09:51:58 +0000

    Update: Implement Adam Optimization Algorithm

--- problems/0049-implement-adam-optimization-algorithm/solution.py
+++ problems/0049-implement-adam-optimization-algorithm/solution.py
@@ -9,32 +9,17 @@ def adam_optimizer(
     epsilon=1e-8,
     num_iterations=10
 ):
-
-    # Copy initial parameters
     x = x0.copy()
-
-    # First moment
     m = np.zeros_like(x)
-
-    # Second moment
     v = np.zeros_like(x)
 
     for t in range(1, num_iterations + 1):
-
-        # Calculate gradient
         g = grad(x)
-
-        # Update first moment
         m = beta1 * m + (1 - beta1) * g
-
-        # Update second moment
         v = beta2 * v + (1 - beta2) * (g ** 2)
-
-        # Bias correction
         m_hat = m / (1 - beta1 ** t)
         v_hat = v / (1 - beta2 ** t)
-
-        # Update parameters
+        #update the parameters 
         x = x - learning_rate * m_hat / (np.sqrt(v_hat) + epsilon)
 
     return x
